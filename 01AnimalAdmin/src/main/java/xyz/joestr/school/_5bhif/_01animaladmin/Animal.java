089beac41b71d2f6ebfa27a021ba8364eabfd332
Joel Strasser <strasser999@gmail.com>
2019-10-21 11:57:49 +0200

    updated animal and main;

--- 01AnimalAdmin/src/main/java/xyz/joestr/school/_5bhif/_01animaladmin/Animal.java
+++ 01AnimalAdmin/src/main/java/xyz/joestr/school/_5bhif/_01animaladmin/Animal.java
@@ -6,15 +6,89 @@
 package xyz.joestr.school._5bhif._01animaladmin;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Objects;
 import org.bson.types.ObjectId;
 
 /**
  *
- * @author Joel
+ * @author admin
  */
 public class Animal {
-    private ObjectId id;
+    private ObjectId _id;
     private String name;
-    transient private LocalDate date;
-    private String details;
+    transient private LocalDate adate;
+    private String adetails;
+
+    public Animal(String name, LocalDate adate, String adetails) {
+        this.name = name;
+        this.adate = adate;
+        this.adetails = adetails;
+    }
+
+    public ObjectId getAid() {
+        return _id;
+    }
+
+    public void setAid(ObjectId aid) {
+        this._id = aid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getAdate() {
+        return adate;
+    }
+
+    public void setAdate(Date adate) {
+        this.adate = adate.toInstant().atZone(ZoneId.of("UTC")).toLocalDate();
+    }
+
+    public String getAdetails() {
+        return adetails;
+    }
+
+    public void setAdetails(String adetails) {
+        this.adetails = adetails;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 17 * hash + Objects.hashCode(this._id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Animal other = (Animal) obj;
+        if (!Objects.equals(this._id, other._id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + "_id=" + _id + ", name=" + name + ", adate=" + adate + ", adetails=" + adetails + '}';
+    }
+
+    
+    
 }
--- 01AnimalAdmin/src/main/java/xyz/joestr/school/_5bhif/_01animaladmin/Main.java
+++ 01AnimalAdmin/src/main/java/xyz/joestr/school/_5bhif/_01animaladmin/Main.java
@@ -11,6 +11,8 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import java.time.LocalDate;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.Date;
 import org.bson.BsonDateTime;
 import org.bson.Document;
 import org.bson.types.ObjectId;
@@ -23,17 +25,25 @@ public class Main {
     
     public static void main(String args[]) {
         
-        /*
-        MongoClient mongoClient = new MongoClient("127.0.0.1", 27017);
-        MongoDatabase mongoDatabase = mongoClient.getDatabase("mydb");
+                MongoClient mongoClient = new MongoClient("127.0.0.1", 27017);
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("animaladmin");
         MongoCollection<Document> animalsCollection = mongoDatabase.getCollection("animals");
+
         Animal a = new Animal("Katze", LocalDate.of(2000, 10, 20), "Meins nix deins");
         Document doc = Document.parse(new Gson().toJson(a));
         doc.append("aDateISO",
                 new BsonDateTime(a.getAdate().atStartOfDay().toInstant(ZoneOffset.UTC).getEpochSecond() * 1000));
         animalsCollection.insertOne(doc);
         a.setAid(((ObjectId) doc.get("_id")));
-        System.out.println("ende");
-        */
+
+        ArrayList<Animal> animals = new ArrayList<>();
+        for (Document d : animalsCollection.find()) {
+            String jsonString = d.toJson();
+            Date tmpDate = d.get("aDateISO", Date.class);
+            Animal animal = new Gson().fromJson(jsonString, Animal.class);
+            animal.setAid((ObjectId) d.get("_id"));
+            animal.setAdate(tmpDate);
+            animals.add(animal);
+        }
     }
 }
