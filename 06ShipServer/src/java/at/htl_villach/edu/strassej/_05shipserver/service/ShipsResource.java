a03697e97eb5a4d27cc8f01f76cf348e2f44b63f
Joel Strasser <strasser999@gmail.com>
2019-12-04 11:53:59 +0100

    initialize 06ShipServer;

---
+++ 06ShipServer/src/java/at/htl_villach/edu/strassej/_05shipserver/service/ShipsResource.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.htl_villach.edu.strassej._05shipserver.service;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author Joel
+ */
+@Path("ships")
+public class ShipsResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of ShipsResource
+     */
+    public ShipsResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of at.htl_villach.edu.strassej._05shipserver.service.ShipsResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of ShipsResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putJson(String content) {
+    }
+}
