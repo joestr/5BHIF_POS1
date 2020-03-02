1667255757c882f59d8e0f8d52e17072b0bc0cf1
Joel Strasser <strasser999@gmail.com>
2020-03-02 21:33:41 +0100

    init 09JSF

---
+++ 09JSF/src/java/pkgData/Login.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgData;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class Login implements Serializable {
+
+	private static final long serialVersionUID = 1094801825228386363L;
+	
+	private String pwd;
+	private String msg;
+	private String user;
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	//validate login
+	public String validateUsernamePassword() {
+		boolean valid = user.equalsIgnoreCase(pwd);
+		if (valid) {
+			return "admin";
+		} else {
+			FacesContext.getCurrentInstance().addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_WARN,
+							"Incorrect Username and Passowrd",
+							"Please enter correct username and Password"));
+			return "login";
+		}
+	}
+
+	//logout event, invalidate session
+	public String logout() {
+		return "login";
+	}
+}
